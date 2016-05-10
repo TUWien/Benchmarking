2cbdf0948fed0c8304c0faa1a530a4a82b1ab1f7
Stefan Fiel <stefan@nomacs.org>
2016-05-10 16:18:05 +0200

    pep 8 indentations

--- lib/writer/authorAndFilesFromMets.py
+++ lib/writer/authorAndFilesFromMets.py
@@ -4,122 +4,133 @@ DMD_SECTION = 'mets:dmdSec'
 NAME_ELEMENT = 'mods:name'
 ROLE_ELEMENT = 'mods:roleTerm'
 
-def getLogicalStructure(xmldoc):
-	from xml.dom import minidom
 
-	print("getting logical structure")
-	itemlist = xmldoc.getElementsByTagName(STRUCT_MAP)
-	for i in itemlist:
-		if i.attributes['TYPE'].value == "LOGICAL":
-			return i
+def getLogicalStructure(xmldoc):
+
+    print("getting logical structure")
+    itemlist = xmldoc.getElementsByTagName(STRUCT_MAP)
+    for i in itemlist:
+        if i.attributes['TYPE'].value == "LOGICAL":
+            return i
+
 
 def getPhysicalStructure(xmldoc):
-	from xml.dom import minidom
 
-	print("getting physical structure")
-	itemlist = xmldoc.getElementsByTagName(STRUCT_MAP)
-	for i in itemlist:
-		if i.attributes['TYPE'].value == "PHYSICAL":
-			return i
+    print("getting physical structure")
+    itemlist = xmldoc.getElementsByTagName(STRUCT_MAP)
+    for i in itemlist:
+        if i.attributes['TYPE'].value == "PHYSICAL":
+            return i
+
 
 def getIds(logStruct):
-	from xml.dom import minidom
 
-	print("getting Ids")
-	itemlist = logStruct.getElementsByTagName(DIV_ELEMENT)
-	for i in itemlist:
-		if i.attributes['TYPE'].value == "document":
-			dmdId = i.attributes['DMDID'].value
-			id = i.attributes['ID'].value
-			print("dmdid:" + dmdId)
-			print("id:" + id)
-			return(dmdId, id)
-	return (-1,-1)
+    print("getting Ids")
+    itemlist = logStruct.getElementsByTagName(DIV_ELEMENT)
+    dmdId = -1
+    id = -1
+    for i in itemlist:
+        if i.attributes['TYPE'].value == "document":
+            dmdId = i.attributes['DMDID'].value
+            id = i.attributes['ID'].value
+            print("dmdid:" + dmdId)
+            print("id:" + id)
+    return(dmdId, id)
 
 
 def getPersonList(dmdId, xmldoc):
-	from xml.dom import minidom
-	print("searching for dmdSec")
+    print("searching for dmdSec")
 
-	itemlist = xmldoc.getElementsByTagName(DMD_SECTION)
+    itemlist = xmldoc.getElementsByTagName(DMD_SECTION)
 
-	for i in itemlist:
-		if i.attributes['ID'].value == dmdId:
-			return i.getElementsByTagName(NAME_ELEMENT)
+    for i in itemlist:
+        if i.attributes['ID'].value == dmdId:
+            return i.getElementsByTagName(NAME_ELEMENT)
 
 
 def getAuthor(personList):
-	from xml.dom import minidom
 
-	name = 0
-	for p in personList:
-		role  = p.getElementsByTagName(ROLE_ELEMENT)
-		for r in role:
-			if len(r.childNodes) > 0:
-				code = r.firstChild.nodeValue
-				if code == "aut":
-					name = p.getElementsByTagName('mods:namePart');
-					for n in name:
-						if n.hasAttribute("type"):
-							if n.attributes['type'].value == "date":
-								print("date:" + n.firstChild.nodeValue)
-						else:
-							name = n.firstChild.nodeValue
+    name = 0
+    for p in personList:
+        role = p.getElementsByTagName(ROLE_ELEMENT)
+        for r in role:
+            if len(r.childNodes) > 0:
+                code = r.firstChild.nodeValue
+                if code == "aut":
+                    name = p.getElementsByTagName('mods:namePart')
+                    for n in name:
+                        if n.hasAttribute("type"):
+                            if n.attributes['type'].value == "date":
+                                print("date:" + n.firstChild.nodeValue)
+                        else:
+                            name = n.firstChild.nodeValue
 
-	return name
+    return name
+
 
 def getFileLinks(id, xmldoc):
-	from xml.dom import minidom
 
-	print("searching for files")
-	itemlist = xmldoc.getElementsByTagName('mets:structLink')
+    print("searching for files")
+    itemlist = xmldoc.getElementsByTagName('mets:structLink')
 
-	fileLinks = []
-	for i in itemlist:
-		for child in i.childNodes:
-			if child.hasAttribute("xlink:from") and child.attributes['xlink:from'].value == id:
-				fileLinks.append(child.attributes['xlink:to'].value)
+    fileLinks = []
+    for i in itemlist:
+        for child in i.childNodes:
+            if (
+                child.hasAttribute("xlink:from") and
+                child.attributes['xlink:from'].value == id
+            ):
+                    fileLinks.append(child.attributes['xlink:to'].value)
 
-	return fileLinks
+    return fileLinks
+
 
 def getFiles(link, physStruct):
-	from xml.dom import minidom
 
-	print("getting files for fileLink:" + link)
-	itemlist = physStruct.getElementsByTagName(DIV_ELEMENT)
-	fileList = []
-	for item in itemlist:
-		if item.hasAttribute("ID") and item.attributes['ID'].value == link:
-			print("id:" + item.attributes['ID'].value)
-			for child in item.childNodes:
-				if child.hasAttribute("FILEID") and str(child.attributes['FILEID'].value).find("MAX", 0 ) > 0:
-					fileList.append(child.attributes['FILEID'].value)
-	return fileList
+    print("getting files for fileLink:" + link)
+    itemlist = physStruct.getElementsByTagName(DIV_ELEMENT)
+    fileList = []
+    for item in itemlist:
+        if item.hasAttribute("ID") and item.attributes['ID'].value == link:
+            print("id:" + item.attributes['ID'].value)
+            for child in item.childNodes:
+                if (
+                    child.hasAttribute("FILEID") and
+                    str(child.attributes['FILEID'].value).find("MAX", 0) > 0
+                ):
+                        fileList.append(child.attributes['FILEID'].value)
+    return fileList
 
 if __name__ == "__main__":
-	from xml.dom import minidom
+    from xml.dom import minidom
 
-	# xmldoc = minidom.parse('E:/Databases/unibas_eManuscripta_firstExamples/emanusbau/913119/913119_mets.xml')
-	# xmldoc = minidom.parse('E:/Databases/unibas_eManuscripta_firstExamples/emanusbau/108192/108192_mets.xml')
-	# xmldoc = minidom.parse('E:/Databases/unibas_eManuscripta_firstExamples/emanusbau/1447421/1447421_mets.xml')
+    # xmldoc = minidom.parse#
+    # ('E:/Databases/unibas_eManuscripta_firstExamples/emanusbau/913119/913119_mets.xml')
+    # xmldoc = minidom.parse
+    # ('E:/Databases/unibas_eManuscripta_firstExamples/emanusbau/108192/108192_mets.xml')
+    xmldoc = minidom.parse(
+        'E:/Databases/unibas_eManuscripta_firstExamples/emanusbau/' +
+        '1447421/1447421_mets.xml')
 
-	# no aut
-	xmldoc = minidom.parse('E:/Databases/unibas_eManuscripta_firstExamples/emanusswa/1013350/1013350_mets.xml')
-	logStruct = getLogicalStructure(xmldoc)
-	(dmdId, id) = getIds(logStruct)
-	print(dmdId)
-	if dmdId != -1 and id != -1:
-		personList = getPersonList(dmdId, xmldoc)
-		author = getAuthor(personList)
-		if author == 0:
-			print("no author found")
-			quit()
-		fileLinks = getFileLinks(id, xmldoc);
-		physStruct = getPhysicalStructure(xmldoc)
-		files = [];
-		for f in fileLinks:
-			file = getFiles(f, physStruct)
-			files.append(file)
+    # no aut
+    # xmldoc = minidom.parse(
+    #     'E:/Databases/unibas_eManuscripta_firstExamples/emanusswa/' +
+    #     '1013350/1013350_mets.xml')
+    logStruct = getLogicalStructure(xmldoc)
+    (dmdId, id) = getIds(logStruct)
+    print(dmdId)
+    if dmdId != -1 and id != -1:
+        personList = getPersonList(dmdId, xmldoc)
+        author = getAuthor(personList)
+        if author == 0:
+            print("no author found")
+            quit()
+        fileLinks = getFileLinks(id, xmldoc)
+        physStruct = getPhysicalStructure(xmldoc)
+        files = []
+        for f in fileLinks:
+            file = getFiles(f, physStruct)
+            files.append(file)
 
-		print("\n\n\nauthor:" + author)
-		print("files:" + str(files))
+        print("\n\n\nauthor:" + author)
+        print("files:" + str(files))

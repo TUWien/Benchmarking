16cc5b98a468dbae1ec8cacb24394dc8a75f2071
Stefan Fiel <stefan@nomacs.org>
2016-05-11 14:49:22 +0200

    added small analyze script

---
+++ lib/writer/analyze.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+
+if __name__ == "__main__":
+    import author
+    import argparse
+    print("generating list")
+
+    parser = argparse.ArgumentParser(description='analyze dataset for writer identification')
+    parser.add_argument('--infile', default="", metavar="text-file",
+                        help="""txt files containgn the full paths to the mets files of the dataset""")
+    parser.add_argument('--outfile', default="", metavar="text-file",
+                        help="""csv file were the different writers with their pages are written""")
+
+    args = parser.parse_args()
+
+    filelist = author.loadfilelist(args.infile)
+    author.generateList(filelist, args.outfile)
+    print("done")
--- lib/writer/author.py
+++ lib/writer/author.py
@@ -5,23 +5,29 @@ def generateList(filelist, outputfile=""):
     import os
     # from writer import parsemets
     errors = ""
+    logmsg = ""
 
     writerlist = {}
     for f in filelist:
         print(f)
+        logmsg += f
         cur_writer = parsemets.parsemets(f)
         if cur_writer != 0:
             if cur_writer.name in writerlist:
                 # check if date correct
                 if cur_writer.date == writerlist[cur_writer.name].date:
                     writerlist[cur_writer.name].addPages(cur_writer.pages)
+                    logmsg += " processed:\t" + str(len(cur_writer.pages)) + " pages added to existing writer\n"
                 else:
                     print("skipping %s because date does not agree" % cur_writer.name)
                     errors += "date mismatch: " + f + "\n"
+                    logmsg += " processed:\t" + "date mismatch\n";
             else:
                 writerlist[cur_writer.name] = cur_writer
+                logmsg += " processed:\t" + str(len(cur_writer.pages)) + " pages added to new writer\n"
         else:
             errors  += "no author detected: " + f + "\n"
+            logmsg += " processed:\t" + "no author detected\n";
 
 
     print("\n\nprinting list")
@@ -44,6 +50,10 @@ def generateList(filelist, outputfile=""):
         f.write(errors)
         f.close()
 
+        logfile = os.path.splitext(outputfile)[0] + '-log.txt'
+        f = open(logfile, 'w')
+        f.write(logmsg)
+        f.close()
 
     print("printing errors:-")
     print(errors)
